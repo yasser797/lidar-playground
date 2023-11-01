7ad7d60a0e14cfff67b3500f6c048ba1ab169353
Yasser Binbisher <57648964+yasser797@users.noreply.github.com>
2023-11-01 15:42:46 -0700

    added write .ply

--- vis_pcd.py
+++ vis_pcd.py
@@ -6,33 +6,33 @@ import os
 
 
 
-def las_to_ply(las_file_path, voxel_size=0.1):
+def las_to_ply(las_file_path, output_ply_path, voxel_size=0.1):
+    
     
     las = laspy.read(las_file_path)
     
+    
     points = np.vstack((las.x, las.y, las.z)).transpose()
+    colors = np.vstack((las.red, las.green, las.blue)).transpose()
     
-    colors = np.vstack((las.red, las.green,
-    las.blue)).transpose()
-    
-    # factor=100
-    # decimated_points = points[::factor]
-        
+  
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points)
-    pcd.colors = o3d.utility.Vector3dVector(colors/65535)
+    pcd.colors = o3d.utility.Vector3dVector(colors / 255)  # if 16-bit color, divide by 65535
     
     
-
-    downpcd = pcd.voxel_down_sample(voxel_size)
-    o3d.visualization.draw_geometries([downpcd])
+    # downpcd = pcd.voxel_down_sample(voxel_size)
+    
+    o3d.io.write_point_cloud(output_ply_path, downpcd)
+    
+    print("Point cloud saved to", output_ply_path)
 
 
 
 def main():
 
       
-    las_file_path = "/mnt/c/Users/ybinb/VS/lidar-processing/2020_Drone_M.las"
+    las_file_path = "lidar-processing/data/santa_monica_col_scans/SaMo_topo_41.las
 
     if not os.path.exists(las_file_path):
         print("File not found:", las_file_path)
@@ -42,16 +42,10 @@ def main():
         print("The file is not a .las file:", las_file_path)
         return
 
-    las_to_ply(las_file_path)
-
-    # las = pylas.read('/mnt/c/Users/ybinb/VS/lidar-processing/2020_Drone_M.las')
-    # num_points = len(las.points)
-    # print('Number of points:', num_points)
- 
     
+    las_to_ply("input.las_file_path", "output.ply")
 
-
-
+    
 
 if __name__ == "__main__":
     main()
