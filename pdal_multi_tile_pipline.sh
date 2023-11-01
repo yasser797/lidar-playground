a123c69630c82cdc825f4b2edc395b7a59380eaf
Zhi He <zhihe122@gmail.com>
2023-11-01 15:54:04 -0700

    added parallel to bash pipeline

--- pdal_multi_tile_pipline.sh
+++ pdal_multi_tile_pipline.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-output_directory="./data/santa_monica_col_scans"
-tiles_directory="./data/tiles"
+function process_tile() {
+    tile_index="$1"
+    
+    output_directory="./data/santa_monica_col_scans"
+    tiles_directory="./data/tiles"
 
-# Check if required directories exist
-if [ ! -d "$output_directory" ] || [ ! -d "$tiles_directory" ]; then
-    echo "Required directories are missing!"
-    exit 1
-fi
+    # Hard-coded input file
+    input_file="./SaMo_topo.laz"
+    file_name=$(basename "$input_file")
+    echo "Processing: $file_name"
 
-# Hard-coded input file
-input_file="./SaMo_topo.laz"
-file_name=$(basename "$input_file")
-echo "Processing: $file_name"
-
-# Base name without extension
-base_name="${file_name%.las}"
-
-for tile_index in {0..91}; do
+    # Base name without extension
+    base_name="${file_name%.las}"
     echo "Processing tile index: $tile_index"
     
     tile_file="$tiles_directory/santa_monica_2016_$tile_index.tif"
@@ -25,7 +20,7 @@ for tile_index in {0..91}; do
     # Check if tile file exists
     if [ ! -f "$tile_file" ]; then
         echo "Tile file does not exist: $tile_file"
-        continue # Skip to the next iteration of the loop
+        return  # Exit the function
     fi
 
     output_file="$output_directory/${base_name}_${tile_index}.las"
@@ -61,4 +56,18 @@ EOF
     if [ $? -ne 0 ]; then
         echo "PDAL pipeline failed for tile index $tile_index"
     fi
-done
+}
+
+export -f process_tile
+
+output_directory="./data/santa_monica_col_scans"
+tiles_directory="./data/tiles"
+
+# Check if required directories exist
+if [ ! -d "$output_directory" ] || [ ! -d "$tiles_directory" ]; then
+    echo "Required directories are missing!"
+    exit 1
+fi
+
+# Run the function process_tile in parallel for each index from 0 to 91
+parallel process_tile ::: {0..91}
